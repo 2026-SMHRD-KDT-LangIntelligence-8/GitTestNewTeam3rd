76d9141c1673734d993330949628f26fe07299d8
BJH <bae090709@gmail.com>
2026-02-25 15:54:15 +0900

    팀장 찐막 엔딩

--- GitTest/src/com/Ex01.java
+++ GitTest/src/com/Ex01.java
@@ -8,7 +8,7 @@ public class Ex01 {
 		System.out.println("팀원2 두번째 커밋");
 		System.out.println("팀원3 충돌 해결");
 		System.out.println("팀원4 마지막은 부담스러워요!");
-		
+		System.out.println("팀장 엔딩요정은 접니다☆");
 	}
 
 }
