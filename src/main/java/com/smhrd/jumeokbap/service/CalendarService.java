089134d9f88000d8e674bc825018f75dc5477628
HSH <67879547hhsshh@gmail.com>
2026-03-27 10:59:45 +0900

    세현커밋!

--- src/main/java/com/smhrd/jumeokbap/service/CalendarService.java
+++ src/main/java/com/smhrd/jumeokbap/service/CalendarService.java
@@ -1,7 +1,20 @@
 package com.smhrd.jumeokbap.service;
 
+import com.smhrd.jumeokbap.repository.SpendingLogRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+
 @Service
+@RequiredArgsConstructor
 public class CalendarService {
+
+    private final BudgetService budgetRepository;
+    private final SpendingLogRepository spendingLogRepository;
+
+
+
 }
